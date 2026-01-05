f3dfe337cf70ae83efa92dac3e94307808f1cdff
jivandinesh-web <jivandinesh@gmail.com>
2026-01-05 12:50:04 +0200

    Create a basic HTTP server to serve index.html

---
+++ server.js
@@ -0,0 +1,6 @@
+conts http=require('http');
+conts fs=require('fs');
+const index = fs.readFileSync('build/index.html');
+http.createServer((req,res) =>{res.writeHead(200,{'Content-Type':'text/html'});
+res.end(index);
+                              }).listen(process.env.PORTll3000);                               
